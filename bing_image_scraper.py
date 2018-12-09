685c29926d37243706fbd5d8937f08ee7e283208
Simeon Trieu <simeon.trieu@gmail.com>
2018-12-09 00:45:27 -0800

    Made YOLO detection optional

--- bing_image_scraper.py
+++ bing_image_scraper.py
@@ -32,37 +32,38 @@ ap.add_argument("--width", required=False,
     help="minimum required width")
 ap.add_argument("--height", required=False,
     help="minimum required height")
-ap.add_argument("-y", "--yolo", required=True,
+ap.add_argument("-y", "--yolo", required=False,
     help="base path to YOLO directory")
 ap.add_argument("--confidence", type=float, default=0.5,
     help="minimum probability to filter weak detections")
 ap.add_argument("--threshold", type=float, default=0.3,
-    help="threshold when applyong non-maxima suppression")
+    help="threshold when applying non-maxima suppression")
 args = vars(ap.parse_args())
 
 """
 # Load the image classifier
 """
 # load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
-LABELS = open(labelsPath).read().strip().split("\n")
+if args["yolo"] is not None:
+    labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+    LABELS = open(labelsPath).read().strip().split("\n")
 
-# initialize a list of colors to represent each possible class label
-np.random.seed(int(time.time()))
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-                           dtype="uint8")
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(int(time.time()))
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                               dtype="uint8")
 
-# derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+    # derive the paths to the YOLO weights and model configuration
+    weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+    configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    # load our YOLO object detector trained on COCO dataset (80 classes)
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    # determine only the *output* layer names that we need from YOLO
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 """
 # Azure parameters
@@ -207,69 +208,72 @@ try:
                 logging.warning("deleting corrupted image: {}".format(p))
                 os.remove(p)
                 continue
-            (H, W) = image.shape[:2]
-            # construct a blob from the input frame and then perform a forward
-            # pass of the YOLO object detector, giving us our bounding boxes
-            # and associated probabilities
-            blob = cv2.dnn.blobFromImage(
-                image,
-                1 / 255.0,  # sigma
-                (416, 416),  # Resize to X by Y
-                swapRB=True,  # BGR to RGB
-                crop=False  # cropping
-            )
-            net.setInput(blob)
-            layerOutputs = net.forward(ln)
 
-            # initialize our lists of detected bounding boxes, confidences,
-            # and class IDs, respectively
-            boxes = []
-            confidences = []
-            classIDs = []
+            if args["yolo"] is not None:
+                (H, W) = image.shape[:2]
+                # construct a blob from the input frame and then perform a forward
+                # pass of the YOLO object detector, giving us our bounding boxes
+                # and associated probabilities
+                blob = cv2.dnn.blobFromImage(
+                    image,
+                    1 / 255.0,  # sigma
+                    (416, 416),  # Resize to X by Y
+                    swapRB=True,  # BGR to RGB
+                    crop=False  # cropping
+                )
+                net.setInput(blob)
+                layerOutputs = net.forward(ln)
 
-            # loop over each of the layer outputs
-            for output in layerOutputs:
-                # loop over each of the detections
-                for detection in output:
-                    # extract the class ID and confidence (i.e., probability)
-                    # of the current object detection
-                    scores = detection[5:]
-                    classID = np.argmax(scores)
-                    confidence = scores[classID]
+                # initialize our lists of detected bounding boxes, confidences,
+                # and class IDs, respectively
+                boxes = []
+                confidences = []
+                classIDs = []
 
-                    # filter out weak predictions by ensuring the detected
-                    # probability is greater than the minimum probability
-                    if confidence > args["confidence"]:
-                        # scale the bounding box coordinates back relative to
-                        # the size of the image, keeping in mind that YOLO
-                        # actually returns the center (x, y)-coordinates of
-                        # the bounding box followed by the boxes' width and
-                        # height
-                        box = detection[0:4] * np.array([W, H, W, H])
-                        (centerX, centerY, width, height) = box.astype("int")
+                # loop over each of the layer outputs
+                for output in layerOutputs:
+                    # loop over each of the detections
+                    for detection in output:
+                        # extract the class ID and confidence (i.e., probability)
+                        # of the current object detection
+                        scores = detection[5:]
+                        classID = np.argmax(scores)
+                        confidence = scores[classID]
 
-                        # use the center (x, y)-coordinates to derive the top
-                        # and and left corner of the bounding box
-                        x = int(centerX - (width / 2))
-                        y = int(centerY - (height / 2))
+                        # filter out weak predictions by ensuring the detected
+                        # probability is greater than the minimum probability
+                        if confidence > args["confidence"]:
+                            # scale the bounding box coordinates back relative to
+                            # the size of the image, keeping in mind that YOLO
+                            # actually returns the center (x, y)-coordinates of
+                            # the bounding box followed by the boxes' width and
+                            # height
+                            box = detection[0:4] * np.array([W, H, W, H])
+                            (centerX, centerY, width, height) = box.astype("int")
 
-                        # update our list of bounding box coordinates,
-                        # confidences, and class IDs
-                        boxes.append([x, y, int(width), int(height)])
-                        confidences.append(float(confidence))
-                        classIDs.append(classID)
+                            # use the center (x, y)-coordinates to derive the top
+                            # and and left corner of the bounding box
+                            x = int(centerX - (width / 2))
+                            y = int(centerY - (height / 2))
 
-            # apply non-maxima suppression to suppress weak, overlapping
-            # bounding boxes
-            idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-                                    args["threshold"])
+                            # update our list of bounding box coordinates,
+                            # confidences, and class IDs
+                            boxes.append([x, y, int(width), int(height)])
+                            confidences.append(float(confidence))
+                            classIDs.append(classID)
 
-            # Test to see if at least one person was detected
-            if not any([LABELS[c] == 'person' for c in classIDs]):
-                logging.warning("deleting image because no people found: {}".format(p))
-                os.remove(p)
-                continue
+                # apply non-maxima suppression to suppress weak, overlapping
+                # bounding boxes
+                idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+                                        args["threshold"])
 
+                # Test to see if at least one person was detected
+                """
+                if not any([LABELS[c] == 'person' for c in classIDs]):
+                    logging.warning("deleting image because no people found: {}".format(p))
+                    os.remove(p)
+                    continue
+                """
             # Found a good image, go to the next
             total += 1
 except requests.HTTPError as e:
